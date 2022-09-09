982a9d4e26dbace9f0943f1a359c3e70e1e8cc9e
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-09-09 15:37:44 -0700

    Reverse lower_case alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'z'; lower_case >= 'a'; lower_case--)
+		putchar(lower_case);
+	putchar('\n');
+
+	return (0);
+}
