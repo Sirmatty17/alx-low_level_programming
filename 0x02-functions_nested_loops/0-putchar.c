0d3c50c95007d84a90a33cbe0991d9a54277578a
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-09-13 15:20:30 -0700

    C, Functions Nested Loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "math.h"
+
+/**
+ * main - print putchar
+ *Return: always 0
+ */
+
+int main(void)
+{
+  putchar(');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putcar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('/n');
+
+  return(0);
+
+}
+  
