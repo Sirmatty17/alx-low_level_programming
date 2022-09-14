aff3ce45cef72b7fb79c746307dfafcf15ccfc08
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-09-14 04:42:26 -0700

    C function nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,9 @@
  *
  *Return: Value of the last digit.
  */
-int print_last_digit(int c)
+int print_last_digit(int n)
 {
-int last_digit = c % 10;
+int last_digit = n % 10;
 
 if (last_digit < 0)
 last_digit *= -1;
