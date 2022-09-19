5def8876241d10418600c8ea083c6ef763a89031
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-09-19 14:48:11 -0700

    Returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *_strien- Finds the length of a string
+ *_strlen- Finds the length of a string
  *@s: String pointer to the string whose length is to be found
  *Return: returns the length of the string
  */
 
-int _strien(char *s)
+int _strlen(char *s)
 {
 int p = 0;
 while (*(s + p) != 0)
