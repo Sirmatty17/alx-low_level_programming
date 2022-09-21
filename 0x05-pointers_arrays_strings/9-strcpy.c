1f2b3a87bcf0257fdf893e33aa5e3833308566c7
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-09-20 22:03:14 -0700

    A pointer that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy the string pointed to by str
+ * including the terminating null byte
+ * buffer pointed to by dest
+ * @dest: the destination
+ * @src: the source
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count)
+		       if (*(src  count) == '\0'
+			       break;
+		count++;
+		
+	}
+	return (dest);
+}
+
+
+
+
