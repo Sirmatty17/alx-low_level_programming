42fd432d482b8573789a20216c2e013222d55ef9
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-09-30 16:22:38 -0700

    Print it's name followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints its name followed by new line
+ * @argc: number of command line
+ * @argv: array hat contains the command line
+ * Return: returns 0 success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
