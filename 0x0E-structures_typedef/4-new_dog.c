560b46296d82554076dc6a9b9809857f18686ee4
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-10-11 08:13:01 -0700

    Creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,23 +12,22 @@
 dog_t *new_dog(cihar *name, float age, char *owner)
 {
 	dog_t *p_dog;
-	int i, nname, nowner;
+	int i, lname, lowner;
 
 	p_dog = malloc(sizeof(*p_dog));
-
 	if (p_dog == NULL || !(name) || !(owner))
 	{
 		free(p_dog);
 		return (NULL);
 	}
-	for (nname = 0; name[nname]; nname++)
-		;
-	for (nowner = 0; owner[nowner]; nowner++)
+	for (lname = 0; name[lname]; lname++)
 		;
 
-	p_dog->name = malloc(nname + 1);
-	p_dog->owner = malloc(nowner + 1);
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
 
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
 	if (!(p_dog->name) || !(p_dog->owner))
 	{
 		free(p_dog->owner);
@@ -36,15 +35,17 @@ dog_t *new_dog(cihar *name, float age, char *owner)
 		free(p_dog);
 		return (NULL);
 	}
-	for (i = 0; i < nname; i++)
+
+	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
+
 	p_dog->age = age;
 
-	for (i = 0; i < nowner; i++)
+	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
 	p_dog->owner[i] = '\0';
 
-	return (p_dog);
+	return (0);
 }
 
