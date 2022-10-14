9750f972c5b91ee96f1a8d9b9b322ac2f296a003
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-10-14 04:55:41 -0700

    function that creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 
 	if (value == NULL)
 		return (NULL);
-	for (i = 0; min <= max; i++)
+	for (i = 0; min <= max; i++, min++)
 		value[i] = min;
 	return (value);
 }
