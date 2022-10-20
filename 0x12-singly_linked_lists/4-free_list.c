431b7fa5d5ff84f19673e82aa36b08e902dacdee
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-10-20 16:11:53 -0700

    function that frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the list
+ * Return: no return
+ */
+
+void free_list(list_t *head)
+{
+	list_t *list;
+
+	while ((list = head) != NULL)
+	{
+		head = head->next;
+		free(list->str);
+		free(list);
+	}
+}
+
