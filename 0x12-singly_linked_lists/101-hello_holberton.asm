ea2db169eae080128a81d4f5ba135f690828a50f
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-10-20 16:26:10 -0700

    assembly that prints Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	ca printf
+
+	mov eax, 0
+	ret
+Footer
+
