1e2b75b158ec0b6ae26da12f811d0f8439c97811
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-10-20 15:44:40 -0700

    C, singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list
+ * print_list - prints all the element of a list
  * @h: singly linked list
- * Return: returns the number of nodes
+ * Return: number of elements in the list
  */
 
 size_t print_list(const list_t *h)
@@ -11,9 +11,12 @@ size_t print_list(const list_t *h)
 	size_t list;
 
 	list = 0;
-
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		list++;
 	}
