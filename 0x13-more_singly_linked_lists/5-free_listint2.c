4dcabc4bc71e8ad01479a71ff0d7fc52bd0ad88c
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-10-25 14:00:48 -0700

    Frees a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,4 +1,25 @@
 #include "lists.h"
 
 /**
- *
+ *free_listint2 - frees a list
+ *@head: head of the list
+ *Return: no return
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *empty;
+	listint_t *latest;
+
+	if (head != NULL)
+	{
+		latest = *head;
+		while ((empty = latest) != NULL)
+		{
+			latest = latest->next;
+			free(empty);
+		}
+		*head = NULL;
+	}
+}
+
