8f2b1a31f73447e92b87504ad240f25682d382f9
Iyanuoluwa <Iyanuoluwagbemiga@gmail.com>
2022-10-27 14:24:12 -0700

    Converts binary to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	for (len--, converted = 1; len >= 0; len--, converted += 2)
+	for (len--, converted = 1; len >= 0; len--, converted *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
